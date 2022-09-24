3936406e6edfe47e0b9f076e2d04972e9538a2f8
shukhrat.kariev <55920277+ShimadzuYo@users.noreply.github.com>
2022-09-24 17:23:28 +0600

    Initial commit

---
+++ GitHubMonitor/GitHubMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace HttpTriggerPractice;
+
+public static class GitHubMonitor
+{
+    [FunctionName("GitHubMonitor")]
+    public static async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        log.LogInformation("Our GitHub Monitor processed an action.");
+
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+        log.LogInformation(requestBody);
+        return new OkResult();
+    }
+}
